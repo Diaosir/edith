3baff369bcbf1669d120c52e153f38e9a2d2f980
Diaosir <835510644@qq.com>
2019-11-18 22:53:43 +0800

    feat: add uri

---
+++ app/web/packages/client-webpack/lib/Uri/uri.test.ts
@@ -0,0 +1,13 @@
+import { URI } from './index'
+describe('uri test', () => {
+    /**
+    .replace(/^-?!+/, "")
+    .replace(/!!+/g, "!")
+    .split("!");
+     */
+    test('new uri by full string', () => {
+        const code = '!!vue-style-loader!css-loader!../../lib/style-compiler/index?{\"vue\":true,\"id\":\"data-v-793be54c\",\"scoped\":false,\"hasInlineConfig\":false}!../../lib/selector?type=styles&index=0&bustCache!./basic.vue'
+        const uri = URI.parse(code);
+        console.log(uri)
+    })
+})
